c69b8c6d454ee23591295f8ab81dcd0d32859773
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-02 17:46:11 +0100

    Implemented 2.3

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -12,7 +12,89 @@ Graph::Graph(int num) : n(num), nodes(num+1) {
 void Graph::addEdge(int src, int dest, int capacity, int duration) {
     if (src<1 || src>n || dest<1 || dest>n)
         return;
-    nodes[src].adj.push_back({dest, capacity, duration});
+    nodes[src].adj.push_back({dest, capacity, duration, 0});
+}
+
+void Graph::initializeResidualNetwork(vector<Node>& residualNetwork) const {
+   for (int i = 1; i <= n; i++) {
+       for (Edge e: nodes[i].adj) {
+           residualNetwork[i].adj.push_back({e.dest, e.capacity, e.duration, e.flow});
+           residualNetwork[e.dest].adj.push_back({i, 0, e.duration, e.flow});
+       }
+   }
+}
+
+vector<int> Graph::findResidualNetworkPath(int src, int dest, vector<Node>& residualNetwork) const {
+    for (Node& n: residualNetwork) {
+        n.visited = false;
+        n.prev = 0;
+    }
+
+    queue<int> q;
+    q.push(src);
+    residualNetwork[src].prev = 0;
+    residualNetwork[src].visited = true;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (Edge e: residualNetwork[u].adj) {
+            if (!residualNetwork[e.dest].visited && e.capacity != 0) {
+                q.push(e.dest);
+                residualNetwork[e.dest].prev = u;
+                residualNetwork[e.dest].visited = true;
+            }
+        }
+    }
+
+    vector<int> path;
+    int currentNode = dest;
+    while (currentNode != 0) {
+        path.push_back(currentNode);
+        currentNode = residualNetwork[currentNode].prev;
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+int Graph::getMaxFlow(int src, int dest) {
+    for (int i = 1; i <= n; i++)
+        for (Edge e: nodes[i].adj)
+            e.flow = 0;
+    vector<Node> residualNetwork(n+1);
+    initializeResidualNetwork(residualNetwork);
+
+    vector<int> path;
+    while ((path=findResidualNetworkPath(src, dest, residualNetwork)).size() > 1) {
+        int minCapacity = INT_MAX;
+        for (int i = 0; i < path.size()-1; i++) {
+            int s = path[i], d = path[i+1];
+            for (Edge e: residualNetwork[s].adj)
+                if (e.dest == d && e.capacity < minCapacity)
+                    minCapacity = e.capacity;
+        }
+
+        for (int i = 0; i < path.size()-1; i++) {
+            int s = path[i], d = path[i+1];
+            //int capacity;
+            for (Edge& e: nodes[s].adj) {
+                if (e.dest == d) {
+                    e.flow += minCapacity;
+                    //capacity = e.capacity;
+                }
+            }
+            for (Edge& e: residualNetwork[s].adj)
+                if (e.dest == d)
+                    e.capacity -= minCapacity;
+            for (Edge& e: residualNetwork[d].adj)
+                if (e.dest == s)
+                    e.capacity += minCapacity;
+        }
+    }
+
+    int maxFlow = 0;
+    for (Edge e: nodes[src].adj)
+        maxFlow += e.flow;
+    return  maxFlow;
 }
 
 void Graph::reuniteGroup(int source, int dest, vector<vector<int>> paths) const {
--- src/Graph.h
+++ src/Graph.h
@@ -2,10 +2,12 @@
 #define _GRAPH_H_
 
 #include "MinHeap.h"
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <queue>
 
 using namespace std;
 
@@ -14,10 +16,13 @@ class Graph {
         int dest;     // Destination node
         int capacity; // An integer capacity
         int duration; // An integer duration
+        int flow;     // flow passing through an edge
     };
 
     struct Node {
         list<Edge> adj; // The list of outgoing edges (to adjacent nodes)
+        bool visited;
+        int prev;  // previous node
     };
 
     int n;              // Graph size (vertices are numbered from 1 to n)
@@ -31,6 +36,9 @@ public:
     // Add edge from source to destination with a certain weight
     void addEdge(int src, int dest, int capcity, int duration);
 
+    void initializeResidualNetwork(vector<Node>& residualNetwork) const;
+    vector<int> findResidualNetworkPath(int src, int dest, vector<Node>& residualNetwork) const;
+    int getMaxFlow(int src, int dest);
     void reuniteGroup(int source, int dest, vector<vector<int>> paths) const;
     void waitTime(int source, int reunite, vector<vector<int>> paths) const;
 };
