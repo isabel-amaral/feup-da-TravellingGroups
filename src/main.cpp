131bce6290ded5e7db0fe05398a8e8c446d0cab1
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-05 12:11:52 +0100

    Changed test files names

--- src/main.cpp
+++ src/main.cpp
@@ -15,39 +15,10 @@ int main() {
     cout << "------------" << endl;
     network.minTranshipments(10, 4); */
 
-/*  testing 2.4/2.5
+/*  testing 2.1/ 2.2
     Graph g(0);
     LoadData data(g);
-    data.loadNetwork(12);
-    Graph network = data.getNetwork();
-
-    vector<vector<int>> paths1;
-    vector<int> v1 = {1,2,4,8,10,12}; // 22 till node 10, 28 till node 12
-    vector<int> v2 = {1,3,5,9,10,12}; // 27 till node 10, 33 till node 12
-    vector<int> v3 = {1,2,4,8,10,11,12}; //27 till node 12
-    paths1.push_back(v1);
-    paths1.push_back(v2);
-    network.reuniteGroup(1, 12, paths1);
-    network.waitTime(1, 10, paths1);
-    cout << endl;
-    paths1.push_back(v3);
-    network.reuniteGroup(1, 12, paths1);
-    network.waitTime(1, 12, paths1);
-    cout << endl;
-
-    vector<vector<int>> paths2;
-    vector<int> v4 = {1,2,4,8,10,12};
-    vector<int> v5 = {1,2,4,8,10,12};
-    paths2.push_back(v4);
-    paths2.push_back(v5);
-    network.reuniteGroup(1, 12, paths2); */
-
-
-/*  testing 2.1/ 2.2*/
-/*
-    Graph g(0);
-    LoadData data(g);
-    data.loadNetwork(14);
+    data.loadNetwork(16);
     Graph network = data.getNetwork();
 
     Graph residualNetwork;
@@ -91,6 +62,32 @@ int main() {
     residualNetwork = network;
     residualNetwork.separateGroup(1, 5, 12,network, 15);*/
 
+/*  testing 2.4/2.5
+    Graph g(0);
+    LoadData data(g);
+    data.loadNetwork(12);
+    Graph network = data.getNetwork();
+
+    vector<vector<int>> paths1;
+    vector<int> v1 = {1,2,4,8,10,12}; // 22 till node 10, 28 till node 12
+    vector<int> v2 = {1,3,5,9,10,12}; // 27 till node 10, 33 till node 12
+    vector<int> v3 = {1,2,4,8,10,11,12}; //27 till node 12
+    paths1.push_back(v1);
+    paths1.push_back(v2);
+    network.reuniteGroup(1, 12, paths1);
+    network.waitTime(1, 10, paths1);
+    cout << endl;
+    paths1.push_back(v3);
+    network.reuniteGroup(1, 12, paths1);
+    network.waitTime(1, 12, paths1);
+    cout << endl;
+
+    vector<vector<int>> paths2;
+    vector<int> v4 = {1,2,4,8,10,12};
+    vector<int> v5 = {1,2,4,8,10,12};
+    paths2.push_back(v4);
+    paths2.push_back(v5);
+    network.reuniteGroup(1, 12, paths2); */
 
     Menu();
     return 0;
